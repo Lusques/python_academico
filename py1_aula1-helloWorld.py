ae84a144667cdf2a89932068bf415bc93d3d544e
Lucas Carlos <89052600+Lusques@users.noreply.github.com>
2021-10-02 01:29:47 -0300

    Delete py1_aula1-helloWorld.py

--- py1_aula1-helloWorld.py
+++
@@ -1,6 +0,0 @@
-#Este programa tem a função de mostrar a mensagem "alo mundo na tela".
-"""
-Assim se faz um comentário em várias linhas.
-Pode-se usar aspas simples ou duplas!
-"""
-print("Alô mundo na tela");
